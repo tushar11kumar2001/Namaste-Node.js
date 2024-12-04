11c4149447c8d266142d49f205a1a22ba5d8560f
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-12-05 03:08:01 +0530

    database

---
+++ database.js
@@ -0,0 +1,8 @@
+const URI = "mongodb+srv://namastenode123:namastenode123@namaste-node.4wmws.mongodb.net/"
+
+//NOTES :
+
+//Go to mongodb
+//Create a free M0 cluster
+//Create a user
+//Install Mongodb compass
